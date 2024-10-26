c4a4e5212e0fdd28faab2120e80e05d0282c3952
tanviryuma <tanvir.yuma@gmail.co>
2024-10-26 12:37:49 +0600

    update 4.1

--- printerclient/build.gradle.kts
+++ printerclient/build.gradle.kts
@@ -36,14 +36,14 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation(fileTree("libs"))
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     // AOP 插件库：https://mvnrepository.com/artifact/org.aspectj/aspectjrt
     implementation("org.aspectj:aspectjrt:1.9.6")
